0bc08bc8860879a21efd8b61ea7a2951cd26d4e2
Ptown2442 <portervaughn93@gmail.com>
2023-10-23 07:07:42 -0700

    google auth initial

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,11 +3,12 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="flex bg-slate-200 p-5">
+    <div className="flex space-x-3 bg-slate-200 p-5">
       <Link className="mr-5" href="/">
         Next.js
       </Link>
       <Link href="/users">Users</Link>
+      <Link href="/api/auth/signin">Sign In With Google</Link>
     </div>
   );
 };
