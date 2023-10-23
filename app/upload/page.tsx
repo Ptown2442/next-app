5ca6241f1258df7511c6b95589af374b8c07258b
Ptown2442 <portervaughn93@gmail.com>
2023-10-22 21:30:04 -0700

    cloudinary initial

---
+++ app/upload/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+import { CldUploadWidget } from "next-cloudinary";
+
+const UploadPage = () => {
+  return (
+    <CldUploadWidget uploadPreset="cngu9skl">
+      {({ open }) => (
+        <button onClick={() => open()} className="btn btn-primary">
+          Upload
+        </button>
+      )}
+    </CldUploadWidget>
+  );
+};
+
+export default UploadPage;
