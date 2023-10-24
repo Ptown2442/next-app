f11590eaf134fd809985f605bf695385a583529a
Ptown2442 <portervaughn93@gmail.com>
2023-10-23 19:07:44 -0700

    sending emails initial

---
+++ emails/WelcomeTemplate.tsx
@@ -0,0 +1,33 @@
+import React, { CSSProperties } from "react";
+import {
+  Html,
+  Body,
+  Container,
+  Text,
+  Link,
+  Preview,
+} from "@react-email/components";
+
+const WelcomeTemplate = ({ name }: { name: string }) => {
+  return (
+    <Html>
+      <Preview>Welcome aboard!</Preview>
+      <Body style={body}>
+        <Container>
+          <Text style={heading}>Hello {name}</Text>
+          <Link href="www.portervaughn.com">www.portervaughn.com</Link>
+        </Container>
+      </Body>
+    </Html>
+  );
+};
+
+const body: CSSProperties = {
+  background: "#fff",
+};
+
+const heading: CSSProperties = {
+  fontSize: "32px",
+};
+
+export default WelcomeTemplate;
